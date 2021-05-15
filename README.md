68a775781bf42e100e4515c6287c139c22c89731
Vladislav Filippov <vvf0807@gmail.com>
2021-05-16 00:09:05 +0300

    Бэкенд 3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # movies-explorer-api
+178.154.207.104 - Публичный IP сервера 
+https://movies-vf.nomoredomains.club - домен 
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 require('dotenv').config();
+const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes');
 const { createUser, login } = require('./controllers/users.js');
 const auth = require('./middlewares/auth.js');
@@ -10,9 +12,14 @@ const errorHandler = require('./middlewares/error-handler.js');
 const { requestLogger, errorLogger } = require('./middlewares/logger.js');
 const { validateCreateUser, validateLogin } = require('./middlewares/validations.js');
 
-const { NODE_ENV } = process.env;
+const { NODE_ENV, DB_CONN } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+mongoose.connect(DB_CONN, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
@@ -22,6 +29,9 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use(requestLogger);
+app.use(limiter);
+app.use(helmet());
+app.use(helmet.hsts());
 app.post('/api/signup', validateCreateUser, createUser);
 app.post('/api/signin', validateLogin, login);
 app.use(auth);
